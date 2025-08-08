28a04e5b2adad2f75bcc58c96dae43e02340a102
Altaf <altafhossen.pro@gmail.com>
2025-08-09 01:00:46 +0600

    Auto: 1 modified - 2025-08-08

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ app.use(cors({
     origin: [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'https://iptv-bd.netlify.app',
         'https://monowartv.netlify.app',
     ],
     credentials: true
